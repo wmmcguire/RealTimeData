4b678c19a07506337ae26e043ca67ccde8b7013c
William McGuire <wmcguire1@hotmail.com>
2019-12-26 13:47:48 -0500

    Update rtdata.py

--- rtdata.py
+++ rtdata.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from time import sleep
 import numpy as np
 
-plt.style.use('fivethirtyeight')
+
 fig = plt.figure()
 ax1=fig.add_subplot(1,1,1)
 plt.title('Sin over Time')
